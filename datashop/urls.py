cd0d614d783a7fa9a2d40ba0e7ffdf4c32d57849
Michael Gyamfi <Jaycehtech>
2022-12-08 14:28:34 +0000

    bundle addition

--- datashop/urls.py
+++ datashop/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('airtime/mtn', views.mtn_request, name="mtn_airtime"),
     path('airtime/airtelTigo', views.airtel_tigo_request, name="tigo_airtime"),
     path('airtime/vodafone', views.voda_request, name="voda_airtime"),
+    path('airtime/glo', views.glo_request, name="glo_airtime"),
     path('send_airtime_mtn/<str:client_ref>/<str:phone>/<str:amount>', views.send_airtime_mtn, name="send_airtime_mtn"),
     path('send_airtime_tigo/<str:client_ref>/<str:phone>/<str:amount>', views.send_airtime_tigo, name="send_airtime_tigo"),
     path('send_airtime_voda/<str:client_ref>/<str:phone>/<str:amount>', views.send_airtime_voda, name="send_airtime_voda"),
+    path('send_airtime_glo/<str:client_ref>/<str:phone>/<str:amount>', views.send_airtime_glo, name="send_airtime_glo"),
     path('thank_you', views.thank_you, name="thank_you"),
     path('failed', views.failed, name="failed"),
 
--- datashop/views.py
+++ datashop/views.py
@@ -20,9 +20,8 @@ def mtn_request(request):
 
             float_amount = float(amount)
             if float_amount == 0.5:
-                percentage = 0.01
-                amount_to_be_charged = float_amount - percentage
-            if float_amount == 1.00:
+                amount_to_be_charged = 0.49
+            elif float_amount == 1.00:
                 percentage = 0.01
                 amount_to_be_charged = float_amount - percentage
             elif float_amount >= 2 and float_amount <= 10:
@@ -112,7 +111,9 @@ def voda_request(request):
             amount_to_be_charged = amount
 
             float_amount = float(amount)
-            if float_amount == 1.00:
+            if float_amount == 0.5:
+                amount_to_be_charged = 0.49
+            elif float_amount == 1.00:
                 percentage = 0.01
                 amount_to_be_charged = float_amount - percentage
             elif float_amount >= 2 and float_amount <= 10:
@@ -199,7 +200,9 @@ def airtel_tigo_request(request):
             amount_to_be_charged = amount
 
             float_amount = float(amount)
-            if float_amount == 1.00:
+            if float_amount == 0.5:
+                amount_to_be_charged = 0.49
+            elif float_amount == 1.00:
                 percentage = 0.01
                 amount_to_be_charged = float_amount - percentage
             elif float_amount >= 2 and float_amount <= 10:
@@ -273,6 +276,94 @@ def send_airtime_tigo(request, client_ref, phone, amount):
 
             break
 
+def glo_request(request):
+    client_ref = 'gds'+str(random.randint(11111111, 99999999))
+
+    if request.method == 'POST':
+        form = AirtimeForm(request.POST)
+        if form.is_valid():
+            phone = str(form.cleaned_data['phone'])
+            amount = str(form.cleaned_data['amount'])
+
+            amount_to_be_charged = amount
+
+            float_amount = float(amount)
+            if float_amount == 0.5:
+                amount_to_be_charged = 0.49
+            if float_amount == 1.00:
+                percentage = 0.01
+                amount_to_be_charged = float_amount - percentage
+            elif float_amount >= 2 and float_amount <= 10:
+                percentage = 0.10
+                amount_to_be_charged = float_amount - percentage
+            elif float_amount >= 11 and float_amount <= 50:
+                percentage = 0.50
+                amount_to_be_charged = float_amount - percentage 
+
+            url = "https://payproxyapi.hubtel.com/items/initiate"
+
+            payload = json.dumps({
+            "totalAmount": amount_to_be_charged,
+            "description": "Test",
+            "callbackUrl": 'https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f',
+            "returnUrl": f'https://bestpay-app-id6nm.ondigitalocean.app/send_airtime_glo/{client_ref}/{phone}/{amount}',
+            "cancellationUrl": "https://www.google.com",
+            "merchantAccountNumber": "2017101",
+            "clientReference": client_ref
+            })
+            headers = {
+            'Authorization': 'Basic VnY3MHhuTTplNTAzYzcyMGYzYzA0N2Q2ODNjYTM3MWQ5YWEwMDZkZg==',
+            'Content-Type': 'application/json'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+            print(response.json())
+            data = response.json()
+            print(data)
+
+            if data["status"] == "Success":
+                messages.info(request, "Successful")
+                checkout = data['data']['checkoutUrl']
+                return redirect(checkout)
+            else:
+                messages.info(request, "Failed. Try again later")
+            return render(request, 'store/layouts/glo.html', context={'form': form})
+    else:
+        form = AirtimeForm()
+    return render(request, 'store/layouts/glo.html', context={'form': form})
+
+
+def send_airtime_glo(request, client_ref, phone, amount):
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    webhook_response = requests.request("GET", "https://webhook.site/token/d53f5c53-eaba-4139-ad27-fb05b0a7be7f/requests?sorting=newest", headers=headers)
+    for request in webhook_response.json()['data']:
+        content = json.loads(request["content"])
+        status = content["Status"]
+        ref = content["Data"]["ClientReference"]
+        if ref == client_ref and status == "Success":
+            glo_url = "https://cs.hubtel.com/commissionservices/2016884/47d88e88f50f47468a34a14ac73e8ab5"
+
+            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
+                    
+            airtime_headers = {
+                'Authorization': 'Basic VnY3MHhuTTplNTAzYzcyMGYzYzA0N2Q2ODNjYTM3MWQ5YWEwMDZkZg==',
+                'Content-Type': 'text/plain'
+            }
+
+            response = requests.request("POST", glo_url, headers=airtime_headers, data=payload)
+
+            if response.status_code == 200:
+                return redirect('thank_you')
+            else:
+                messages.info(request, "Try again Later")
+                    
+
+            return render(request, 'store/layouts/glo.html', context={'form': form})
+
+            break
 
 def thank_you(request):
     return render(request, "store/layouts/thanks.html")
