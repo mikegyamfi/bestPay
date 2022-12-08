e4aad725b498c91fcbd49a99e233a097ccb8f0a1
Michael Gyamfi <Jaycehtech>
2022-12-08 14:54:51 +0000

    bundle addition

--- datashop/forms.py
+++ datashop/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AirtimeForm(forms.Form):
-    phone = forms.IntegerField(label='Phone Number', required=True)
+    phone = forms.IntegerField(label='Phone Number', required=True, help_text="Number must start with 233 and exclude 0 after 233. Eg. 2332XXXXXXXX")
     # provider = forms.ChoiceField(label='Provider', choices=[(1, 'MTN'), (2, 'AirtelTigo'), (3, 'Vodafone')], required=True)
     amount = forms.FloatField(label='Amount', required=True)
 
@@ -14,10 +14,13 @@ class AirtimeForm(forms.Form):
         if phone:
             if str(phone)[:3] != "233":
                 raise forms.ValidationError('Number must start with country code: Eg. 233XXXXXXXXX')
+            if len(str(phone)) != 12:
+                raise forms.ValidationError('Check your number and try again. You may want to exclude the "0" after 233')
+
 
 
 class BundleForm(forms.Form):
-    phone = forms.IntegerField(label='Phone Number', required=True)
+    phone = forms.IntegerField(label='Phone Number', required=True, help_text="Number must start with 233 and exclude 0 after 233. Eg. 2332XXXXXXXX")
 
     def clean(self):
         cleaned_data = super(BundleForm, self).clean()
@@ -27,3 +30,5 @@ class BundleForm(forms.Form):
         if phone:
             if str(phone)[:3] != "233":
                 raise forms.ValidationError('Number must start with country code: Eg. 233XXXXXXXXX')
+            if len(str(phone)) != 12:
+                raise forms.ValidationError('Check your number and try again. You may want to exclude the "0" after 233')
