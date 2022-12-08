3d450f7479ef6956a052e68d975a3214420b3ee0
Michael Gyamfi <Jaycehtech>
2022-12-08 08:36:09 +0000

    percentages

--- datashop/views.py
+++ datashop/views.py
@@ -34,7 +34,7 @@ def mtn_request(request):
             payload = json.dumps({
             "totalAmount": amount_to_be_charged,
             "description": "Test",
-            "callbackUrl": 'https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569',
+            "callbackUrl": 'https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f',
             "returnUrl": f'https://bestpay-app-id6nm.ondigitalocean.app/send_airtime_mtn/{client_ref}/{phone}/{amount}',
             "cancellationUrl": "https://www.google.com",
             "merchantAccountNumber": "2017101",
@@ -66,7 +66,7 @@ def send_airtime_mtn(request, client_ref, phone, amount):
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
-    webhook_response = requests.request("GET", "https://webhook.site/token/092193ad-e5e7-4f17-a472-3442a8670569/requests?sorting=newest", headers=headers)
+    webhook_response = requests.request("GET", "https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f/requests?sorting=newest", headers=headers)
     for request in webhook_response.json()['data']:
         try:
             content = json.loads(request["content"])
@@ -77,7 +77,7 @@ def send_airtime_mtn(request, client_ref, phone, amount):
         if ref == client_ref and status == "Success":
             mtn_url = "https://cs.hubtel.com/commissionservices/2016884/fdd76c884e614b1c8f669a3207b09a98"
 
-            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
+            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
                     
             headers = {
                 'Authorization': 'Basic VnY3MHhuTTplNTAzYzcyMGYzYzA0N2Q2ODNjYTM3MWQ5YWEwMDZkZg==',
@@ -124,7 +124,7 @@ def voda_request(request):
             payload = json.dumps({
             "totalAmount": amount_to_be_charged,
             "description": "Test",
-            "callbackUrl": 'https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569',
+            "callbackUrl": 'https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f',
             "returnUrl": f'https://bestpay-app-id6nm.ondigitalocean.app/send_airtime_voda/{client_ref}/{phone}/{amount}',
             "cancellationUrl": "https://www.google.com",
             "merchantAccountNumber": "2017101",
@@ -157,7 +157,7 @@ def send_airtime_voda(request, client_ref, phone, amount):
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
-    webhook_response = requests.request("GET", "https://webhook.site/token/092193ad-e5e7-4f17-a472-3442a8670569/requests?sorting=newest", headers=headers)
+    webhook_response = requests.request("GET", "https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f/requests?sorting=newest", headers=headers)
     for request in webhook_response.json()['data']:
         content = json.loads(request["content"])
         status = content["Status"]
@@ -165,7 +165,7 @@ def send_airtime_voda(request, client_ref, phone, amount):
         if ref == client_ref and status == "Success":
             voda_url = "https://cs.hubtel.com/commissionservices/2016884/f4be83ad74c742e185224fdae1304800"
 
-            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
+            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
                     
             headers = {
                 'Authorization': 'Basic VnY3MHhuTTplNTAzYzcyMGYzYzA0N2Q2ODNjYTM3MWQ5YWEwMDZkZg==',
@@ -211,7 +211,7 @@ def airtel_tigo_request(request):
             payload = json.dumps({
             "totalAmount": amount_to_be_charged,
             "description": "Test",
-            "callbackUrl": 'https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569',
+            "callbackUrl": 'https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f',
             "returnUrl": f'https://bestpay-app-id6nm.ondigitalocean.app/send_airtime_tigo/{client_ref}/{phone}/{amount}',
             "cancellationUrl": "https://www.google.com",
             "merchantAccountNumber": "2017101",
@@ -243,7 +243,7 @@ def send_airtime_tigo(request, client_ref, phone, amount):
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
-    webhook_response = requests.request("GET", "https://webhook.site/token/092193ad-e5e7-4f17-a472-3442a8670569/requests?sorting=newest", headers=headers)
+    webhook_response = requests.request("GET", "https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f/requests?sorting=newest", headers=headers)
     for request in webhook_response.json()['data']:
         content = json.loads(request["content"])
         status = content["Status"]
@@ -251,7 +251,7 @@ def send_airtime_tigo(request, client_ref, phone, amount):
         if ref == client_ref and status == "Success":
             tigo_url = "https://cs.hubtel.com/commissionservices/2016884/dae2142eb5a14c298eace60240c09e4b"
 
-            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/d53f5c53-eaba-4139-ad27-fb05b0a7be7f\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
+            payload = "{\r\n    \"Destination\": " + phone + ",\r\n    \"Amount\": " + amount + ",\r\n    \"CallbackUrl\": \"https://webhook.site/092193ad-e5e7-4f17-a472-3442a8670569\",\r\n    \"ClientReference\": \"TestEVD01027\"\r\n}"
                     
             airtime_headers = {
                 'Authorization': 'Basic VnY3MHhuTTplNTAzYzcyMGYzYzA0N2Q2ODNjYTM3MWQ5YWEwMDZkZg==',
